e12f556c7dd84d5fd8c1d698e76f1f97e82308fe
Deias <saied_am@hotmail.com>
2022-05-17 19:29:00 +0200

    serializer for Client model

--- src/clients/serializers.py
+++ src/clients/serializers.py
@@ -2,22 +2,8 @@ from rest_framework import serializers
 from clients.models import Client
 
 
-class ClientListSerializer(serializers.ModelSerializer):
-    """A serializer for client objects.
-
-    used for list serialization.
-    """
-
-    class Meta:
-        model = Client
-        fields = ("compagny_name", "first_name", "last_name")
-
-
-class ClientDetailSerializer(serializers.ModelSerializer):
-    """A serializer for client objects.
-
-    used for detail serialization.
-    """
+class ClientSerializer(serializers.ModelSerializer):
+    """A serializer for client objects."""
 
     class Meta:
         model = Client
