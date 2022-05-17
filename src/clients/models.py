3701799a2f597efb5d873d2059ae97aca070a5d5
Deias <saied_am@hotmail.com>
2022-05-17 19:11:58 +0200

    adding validator for the mobile and phone fields of the client model

--- src/clients/models.py
+++ src/clients/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import RegexValidator
 from django.utils.translation import gettext_lazy as _
 
 
@@ -8,10 +9,14 @@ class Client(models.Model):
     first_name = models.CharField(_("first name"), max_length=25)
     last_name = models.CharField(_("last name"), max_length=25)
     email = models.EmailField(max_length=100, unique=True)
+    phone_regex = RegexValidator(
+        regex=r'^\+?1?\d{9,15}$',
+        message="Phone number must be entered in the format:"
+        + " '+999999999'. Up to 15 digits allowed.")
     phone = models.CharField(_("phone number"), max_length=20,
-                             blank=True, null=True)
+                             validators=[phone_regex], blank=True, null=True)
     mobile = models.CharField(_("mobile number"), max_length=20,
-                              blank=True, null=True)
+                              validators=[phone_regex], blank=True, null=True)
     compagny_name = models.CharField(_("compagny name"), max_length=250,
                                      unique=True)
     date_created = models.DateTimeField(auto_now_add=True)
