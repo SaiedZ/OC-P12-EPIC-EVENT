1216534ff7b841214d4c28f861cd21f577d2465e
Deias <saied_am@hotmail.com>
2022-05-19 11:50:17 +0200

    adding view, urls for clients

--- src/clients/views.py
+++ src/clients/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from clients import models
+from clients.serializers import ClientSerializer
+
+
+class ClientViewSet(viewsets.ModelViewSet):
+
+    serializer_class = ClientSerializer
+
+    def get_queryset(self):
+        """
+        Get the list of items for this view.
+        """
+        return models.Client.objects.all()
