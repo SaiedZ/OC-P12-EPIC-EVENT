63feb3bb586783b166c8c3df585cb74d3c9a8bb7
Deias <saied_am@hotmail.com>
2022-05-19 17:36:48 +0200

    adding the sign contract methode to the ContractViewSets view

--- src/contracts/views.py
+++ src/contracts/views.py
@@ -1,4 +1,7 @@
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework import status
+from rest_framework.response import Response
 
 from contracts import models
 from contracts.serializers import ContractSerializer
@@ -13,3 +16,14 @@ class ContractViewSet(viewsets.ModelViewSet):
         Get the list of items for this view.
         """
         return models.Contract.objects.all()
+
+    @action(detail=True, methods=['post'])
+    def sign_contract(self, request, pk=None):
+        contract = self.get_object()
+        if not contract.status:
+            contract.status = True
+            contract.save()
+            content = {'message': 'Contract signed.'}
+            return Response(content, status=status.HTTP_201_CREATED)
+        content = {"message": "Contract already signed."}
+        return Response(content, status=status.HTTP_409_CONFLICT)
