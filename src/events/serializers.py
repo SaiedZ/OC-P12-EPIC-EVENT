b985ac86ebd48133bfc9fea2c0465e641e892005
Deias <saied_am@hotmail.com>
2022-05-20 22:14:17 +0200

    serializers for the event model

--- src/events/serializers.py
+++ src/events/serializers.py
@@ -1,14 +1,48 @@
+from multiprocessing import context
 from rest_framework import serializers
 
 from events.models import EventStatus, Event
+from contracts.serializers import ContractSerializer
+from teams.serializers import CRMUserSerializer
 
 
 class EventSerializer(serializers.ModelSerializer):
 
+    def __init__(self, *args, **kwargs):
+        """Initialize and if the view action is `list` or `retrieve`
+        the contract field will be set to `ContractSerializer`.
+
+        This will give extras informations about the contract.
+        """
+        super().__init__(*args, **kwargs)
+        if self.context['view_action'] == 'retrieve':
+            self.fields['contract'] = ContractSerializer()
+            self.fields['support_contact'] = CRMUserSerializer(
+                context=self.context)
+        if self.context['view_action'] == 'list':
+            self.fields['support_contact'] = serializers.StringRelatedField()
+            self.fields['contract'] = serializers.StringRelatedField()
+
+    client_name = serializers.CharField(read_only=True,
+                                        source=str('contract.client'))
+
     class Meta:
         model = Event
-        fields = ['id', 'contract', 'support_contact', 'status', 'attendees',
-                  'event_date', 'notes', 'date_created', 'date_updated']
+        fields = ['id', 'contract', 'support_contact', 'client_name',
+                  'status', 'attendees', 'event_date', 'notes',
+                  'date_created', 'date_updated']
+
+    def to_representation(self, instance):
+        response = super().to_representation(instance)
+        # response['contract'] = ContractSerializer(instance.contract).data
+        response['status'] = instance.status.name.capitalize()
+        response['event_date'] = instance.event_date.strftime(
+            "%H:%M:%S %d-%m-%Y")
+        response['date_created'] = instance.date_created.strftime(
+            "%H:%M:%S %d-%m-%Y")
+        response['date_updated'] = instance.date_updated.strftime(
+            "%H:%M:%S %d-%m-%Y")
+        return response
 
 
 class EventStatusSerializer(serializers.ModelSerializer):
