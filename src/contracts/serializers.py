16fb2b54c6d49f51cd2078b4f9de6e5c4f34684c
Deias <saied_am@hotmail.com>
2022-05-21 20:01:12 +0200

    improving serializers for contract to give more detail for list

--- src/contracts/serializers.py
+++ src/contracts/serializers.py
@@ -1,9 +1,26 @@
 from rest_framework import serializers
 
 from contracts.models import Contract
+from teams.serializers import CRMUserSerializer
+from clients.serializers import ClientSerializer
 
 
 class ContractSerializer(serializers.ModelSerializer):
+
+    def __init__(self, *args, **kwargs):
+        """Initialize and if the view action is `list` or `retrieve`
+        the contract field will be set to `ContractSerializer`.
+
+        This will give extras informations about the contract.
+        """
+        super().__init__(*args, **kwargs)
+        if self.context['view_action'] == 'retrieve':
+            self.fields['client'] = ClientSerializer()
+            self.fields['sales_contact'] = CRMUserSerializer(
+                context=self.context)
+        if self.context['view_action'] == 'list':
+            self.fields['client'] = serializers.StringRelatedField()
+            self.fields['sales_contact'] = serializers.StringRelatedField()
 
     class Meta:
         model = Contract
@@ -12,3 +29,11 @@ class ContractSerializer(serializers.ModelSerializer):
         extra_kwargs = {
             'status': {'read_only': True},
         }
+
+    def to_representation(self, instance):
+        response = super().to_representation(instance)
+        response['date_created'] = instance.date_created.strftime(
+            "%H:%M:%S %d-%m-%Y")
+        response['date_updated'] = instance.date_updated.strftime(
+            "%H:%M:%S %d-%m-%Y")
+        return response
