cca8f83da95e6ef7a7196f9a6eae71d2b8137b9a
Deias <saied_am@hotmail.com>
2022-05-23 12:48:58 +0200

    refactoring permissions classes

--- src/clients/permissions.py
+++ src/clients/permissions.py
@@ -4,26 +4,22 @@ from rest_framework.permissions import BasePermission
 class HasClientPermissions(BasePermission):
 
     def has_permission(self, request, view):
-        if request.user.is_authenticated and hasattr(request.user, 'team'):
+        if request.user.is_authenticated and request.user.team is not None:
             return request.user.team.name == "Sale"
         return False
 
     def has_object_permission(self, request, view, obj):
-        if request.user.is_authenticated and hasattr(request.user, 'team'):
-            return request.user.team.name == "Sale"
-        return False
+        return request.user.team.name == "Sale"
 
 
 class IsManager(BasePermission):
 
     def has_permission(self, request, view):
-        if request.user.is_authenticated and hasattr(request.user, 'team'):
+        if request.user.is_authenticated and request.user.team is not None:
             return request.user.team.name == "Management" \
-                and request.method not in ('POST', 'DELETE')
+                and request.method != "POST"
         return False
 
     def has_object_permission(self, request, view, obj):
-        if request.user.is_authenticated and hasattr(request.user, 'team'):
-            return request.user.team.name == "Management" \
-                and request.method not in ('POST', 'DELETE')
-        return False
+        return request.user.team.name == "Management" \
+            and request.method not in ('DELETE',)
--- src/contracts/permissions.py
+++ src/contracts/permissions.py
@@ -1,21 +1,14 @@
-from rest_framework.permissions import BasePermission,SAFE_METHODS
+from rest_framework.permissions import BasePermission
 
 
 class ContractPermissionSafeAndPost(BasePermission):
 
     def has_permission(self, request, view):
-        print("has_permission", request.method)
-        if request.user.is_authenticated:
-            return hasattr(request.user, "team") \
-                and request.user.team.name in ["Sale", "Management"] \
-                and request.method not in ["PUT", "PATCH", "DELETE"]
+        if request.user.is_authenticated and request.user.team is not None:
+            return request.user.team.name in ["Sale", "Management"]
         return False
 
     def has_object_permission(self, request, view, obj):
-        print("has_object_permission", request.method)
-        print(view.action)
-        if request.method in SAFE_METHODS:
-            if view.action == "sign_contract":
-                return request.user.id == obj.sales_contact
+        if view.action == "retrieve":
             return True
-        return request.user.id == obj.sales_contact
+        return request.user == obj.sales_contact
