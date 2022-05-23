8e25e06c1c276d9eb2c22cc924ca4bb1f7db4a91
Deias <saied_am@hotmail.com>
2022-05-23 12:53:27 +0200

    Refatoring permissions for users and teasm

--- src/teams/permissions.py
+++ src/teams/permissions.py
@@ -4,13 +4,7 @@ from rest_framework.permissions import BasePermission, SAFE_METHODS
 class IsAuthenticatedAndSuperUserOrFromManagement(BasePermission):
 
     def has_permission(self, request, view):
-        if request.user.is_authenticated and hasattr(request.user, 'team'):
-            return request.user.is_superuser \
-                or request.user.team.name == "Management"
-        return False
-
-    def has_object_permission(self, request, view, obj):
-        if request.user.is_authenticated and hasattr(request.user, 'team'):
+        if request.user.is_authenticated and request.user.team is not None:
             return request.user.is_superuser \
                 or request.user.team.name == "Management"
         return False
@@ -19,7 +13,7 @@ class IsAuthenticatedAndSuperUserOrFromManagement(BasePermission):
 class IsAuthenticatedAndSuperUserOrManagerForSafeMethods(BasePermission):
 
     def has_permission(self, request, view):
-        if request.user.is_authenticated and hasattr(request.user, 'team'):
+        if request.user.is_authenticated and request.user.team is not None:
             return request.user.is_superuser \
                 or request.user.team.name == "Management" \
                 and request.method in SAFE_METHODS
@@ -27,6 +21,4 @@ class IsAuthenticatedAndSuperUserOrManagerForSafeMethods(BasePermission):
         return False
 
     def has_object_permission(self, request, view, obj):
-        if request.user.is_authenticated and hasattr(request.user, 'team'):
-            return request.user.is_superuser
-        return False
+        return request.user.is_superuser
