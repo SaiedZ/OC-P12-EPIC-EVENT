be14e970f5aa075a0209b9b8d3cab69d02fc85f7
Deias <saied_am@hotmail.com>
2022-05-24 11:00:52 +0200

    adding LogoutView and LogoutAllView

--- src/authentication/views.py
+++ src/authentication/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions, status
+from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import (RefreshToken,
+                                             TokenError,
+                                             OutstandingToken,
+                                             BlacklistedToken)
+from rest_framework.response import Response
 
-# Create your views here.
+
+class LogoutView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request):
+        try:
+            refresh_token = request.data["refresh_token"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+
+            return Response(status=status.HTTP_205_RESET_CONTENT)
+        except TokenError:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+class LogoutAllView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request):
+        tokens = OutstandingToken.objects.filter(user_id=request.user.id)
+        for token in tokens:
+            t, _ = BlacklistedToken.objects.get_or_create(token=token)
+
+        return Response(status=status.HTTP_205_RESET_CONTENT)
