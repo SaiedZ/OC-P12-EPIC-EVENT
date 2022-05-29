c73b978023ff92f999de2ee13999ea0c6070a60a
Deias <saied_am@hotmail.com>
2022-05-29 22:59:41 +0200

    chore: disabling the BasicAuthentication

--- src/CRM/settings.py
+++ src/CRM/settings.py
@@ -160,8 +160,8 @@ REST_FRAMEWORK = {
     ],
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework_simplejwt.authentication.JWTAuthentication",
-        "rest_framework.authentication.BasicAuthentication",
         "rest_framework.authentication.SessionAuthentication",
+        # "rest_framework.authentication.BasicAuthentication",
     ],
     "DEFAULT_FILTER_BACKENDS": [
         "url_filter.integrations.drf.DjangoFilterBackend",
