dce891be6bac92b53d7a4754a0e55615b5b9094b
Deias <saied_am@hotmail.com>
2022-05-29 19:18:52 +0200

    style: improving the style of the admin.py

--- src/teams/admin.py
+++ src/teams/admin.py
@@ -8,25 +8,48 @@ class UserAdminConfig(UserAdmin):
 
     search_fields = ("first_name", "email", "username", "team")
     list_filter = ("email", "username", "team")
-    ordering = ("team", "first_name",)
-    list_display = ("team", "first_name", "last_name", "email",
-                    "username", "is_active", "is_staff",
-                    "phone", "mobile")
+    ordering = (
+        "team",
+        "first_name",
+    )
+    list_display = (
+        "first_name",
+        "last_name",
+        "email",
+        "team",
+        "username",
+        "is_active",
+        "is_staff",
+        "phone",
+        "mobile",
+    )
     fieldsets = (
         (None, {"fields": ("email", "username")}),
         ("Departement", {"fields": ("team",)}),
         ("Permissions", {"fields": ("is_staff", "is_active")}),
-        ("Personal informations", {"fields": (
-            "first_name", "last_name", "phone", "mobile")}),
+        (
+            "Personal informations",
+            {"fields": ("first_name", "last_name", "phone", "mobile")},
+        ),
     )
     add_fieldsets = (
-        (None, {
-            "classes": ("wide",),
-            "fields": ("email", "username", "team",
-                       "first_name", "last_name",
-                       "password1", "password2",
-                       "phone", "mobile")}
-         ),
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "username",
+                    "team",
+                    "first_name",
+                    "last_name",
+                    "password1",
+                    "password2",
+                    "phone",
+                    "mobile",
+                ),
+            },
+        ),
     )
 
 
