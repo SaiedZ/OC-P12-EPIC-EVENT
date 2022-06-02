eb0a80bf369f1ce2f706e762a1adbe87905efd11
Deias <saied_am@hotmail.com>
2022-06-02 17:42:20 +0200

    chore: adding templates and static config

--- src/CRM/settings.py
+++ src/CRM/settings.py
@@ -79,7 +79,7 @@ ROOT_URLCONF = 'CRM.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -147,6 +147,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- src/CRM/urls.py
+++ src/CRM/urls.py
@@ -1,18 +1,7 @@
-"""CRM URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+CRM URL Configuration
+"""
+
 from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import (
@@ -20,6 +9,8 @@ from drf_spectacular.views import (
     SpectacularRedocView,
     SpectacularSwaggerView
 )
+from django.conf.urls.static import static
+from django.conf import settings
 
 from CRM.admin import crm_admin_site
 
@@ -40,4 +31,4 @@ urlpatterns = [
     path('api/schema/redoc/',
          SpectacularRedocView.as_view(url_name='schema'), name='redoc'
          ),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
